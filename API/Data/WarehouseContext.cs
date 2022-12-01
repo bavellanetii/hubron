03c251b4b6c20e5d19d55b702f0a72c3102a3b33
Brandon Avellanet <bavellanetii@gmail.com>
2022-12-01 18:57:59 +0000

    Setting up entity framework

---
+++ API/Data/WarehouseContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class WarehouseContext : DbContext
+    {
+        public WarehouseContext(DbContextOptions<WarehouseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Properties { get; set; }
+    }
+}
