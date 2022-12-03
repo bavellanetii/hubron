d480a3e57a8a2f38c430699010ebc6b7233239b1
Brandon Avellanet <bavellanetii@gmail.com>
2022-12-03 12:43:33 +0000

    Added Repository Methods

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,18 +15,19 @@ namespace API.Controllers
 
     public class ProductsController : ControllerBase
     {
-        private readonly WarehouseContext _context;
-        public ProductsController(WarehouseContext context)
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
+            
         }
 
         [HttpGet]
         public async Task <ActionResult<List<Product>>> GetProducts()
         {
             //this will be a list of products
-            var products = await _context.Products.ToListAsync();
-
+            var products = await _repo.GetProductsAsync();
             return Ok(products);
         }
 
@@ -33,7 +35,7 @@ namespace API.Controllers
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             //this will be a single product
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
