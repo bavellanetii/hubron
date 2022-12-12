712f8f0253c99a7a9aacc243dbb5d1e3127e7ac7
Brandon Avellanet <bavellanetii@gmail.com>
2022-12-12 19:02:06 +0000

    backup before trying hubron way of ordering lot numbers

--- Core/Specifications/ProductsInFullSpecification.cs
+++ Core/Specifications/ProductsInFullSpecification.cs
@@ -9,13 +9,40 @@ namespace Core.Specifications
 {
     public class ProductsInFullSpecification : BaseSpecification<Product>
     {
-        public ProductsInFullSpecification()
+        public ProductsInFullSpecification(string sort)
         {
             AddInclude(x => x.Grade);
             AddInclude(x => x.LotNumber);
             AddInclude(x => x.Packaging);
             AddInclude(x => x.Status);
             AddInclude(x => x.Warehouse);
+            
+            AddOrderBy(x => x.Grade.Name);
+            
+            
+
+            if(!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "warehouse":
+                        AddOrderBy(w => w.Warehouse.Name);
+                        break;
+                    case "lotNumberAscending":
+                        AddOrderBy(l => l.LotNumber.Year);
+                        AddOrderBy(l => l.LotNumber.Name);
+                        break;
+                    case "packaging":
+                        AddOrderBy(p => p.Packaging.Name);
+                        break;
+                    case "status":
+                        AddOrderBy(s => s.Status.Name);
+                        break;
+                    default:
+                        AddOrderBy(x => x.Grade.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsInFullSpecification(int id)
