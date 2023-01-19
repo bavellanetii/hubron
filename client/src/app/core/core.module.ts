3e3ad6ca19f3b7989bdbffefc7921225decdbdb0
Brandon Avellanet <bavellanetii@gmail.com>
2023-01-19 17:50:03 +0000

    added section header

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { RouterModule } from '@angular/router';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, SideBarComponent],
+  declarations: [NavBarComponent, SideBarComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [NavBarComponent, SideBarComponent]
+  exports: [NavBarComponent, SideBarComponent, SectionHeaderComponent]
 })
 export class CoreModule { }
